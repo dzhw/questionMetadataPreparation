da5f0647195bcddc3f0fb629d305366ac4cf7af5
René Reitmann <reitmann@dzhw.eu>
2019-07-12 14:30:01 +0200

    dzhw/metadatamanagement#2018, dzhw/metadatamanagement#2045 reimplement the major stuff

---
+++ R/convert_handcrafted_questionnaire_to_mdm_format.R
@@ -0,0 +1,219 @@
+#' Convert handcrafted questionnaires to MDM format
+#'
+#' This script converts the handcrafted questionnaires which contains an excel
+#' and many images of several questionnaires into the format which can be
+#' uploaded to the MDM. The handcrafted questionnaire must have the following
+#' format:
+#' |--questions
+#'   |--questions*.xlsx (two sheets, questions and images)
+#'   |--Bilder
+#'     |--png
+#'       |--ins1
+#'         |--5_1.png (must match the fileName in the images excel sheet)
+#'         |--5_2.png (must match the fileName in the images excel sheet)
+#'       |--ins2
+#'         |--5_1.png (must match the fileName in the images excel sheet)
+#'         |--5_2.png (must match the fileName in the images excel sheet)
+#'
+#' @param input_directory Input path, e.g. "./questions"
+#' @param output_directory Output directory, e.g. "./mdm/questions", will
+#' be created if it does not exist or will be overwritten otherwise
+#' @param images_subdirectory Path relativ to input_directory containing the
+#' images, e.g. "Bilder/png"
+#' @export
+convert_handcrafted_questionnaires_to_mdm_format <- function(
+  input_directory = file.path(".", "questions"),
+  output_directory = file.path(".", "mdm", "questions"),
+  images_subdirectory = file.path("Bilder", "png")) {
+  create_empty_directory(output_directory)
+
+  xlsx_file <- dir(input_directory, pattern = "*.xlsx", full.names = TRUE)[[1]]
+
+  write_question_jsons(xlsx_file, output_directory)
+
+  write_question_images(xlsx_file, input_directory, output_directory,
+    images_subdirectory)
+}
+
+write_question_jsons <- function(xlsx_file, output_directory) {
+  message("Read excel file: sheet questions")
+  excel <- read_and_trim_excel(xlsx_file, sheet = "questions")
+  # trim later
+  excel <- trim_list_cols(excel, col1 = "successorNumbers", col2 = "conceptIds")
+
+  # for all questions in excel
+  for (i in rownames(excel)) {
+    que <- new.env(hash = TRUE, parent = emptyenv())
+
+    que[["indexInInstrument"]] <- jsonlite::unbox(
+      as.numeric(excel[i, "indexInInstrument"])
+    )
+    que[["questionText"]] <- new.i18n_string(
+      excel[i, "questionText.de"], excel[i, "questionText.en"]
+    )
+    que[["instruction"]] <- new.i18n_string(
+      excel[i, "instruction.de"], excel[i, "instruction.en"]
+    )
+    que[["introduction"]] <- new.i18n_string(
+      excel[i, "introduction.de"], excel[i, "introduction.en"]
+    )
+    que[["type"]] <- new.i18n_string(
+      excel[i, "type.de"], excel[i, "type.en"]
+    )
+    que[["additionalQuestionText"]] <- new.i18n_string(
+      excel[i, "additionalQuestionText.de"],
+      excel[i, "additionalQuestionText.en"]
+    )
+    que[["topic"]] <- new.i18n_string(
+      excel[i, "topic.de"], excel[i, "topic.en"]
+    )
+    que[["annotations"]] <- new.i18n_string(
+      excel[i, "annotations.de"], excel[i, "annotations.en"]
+    )
+    que[["successorNumbers"]] <- list_attribute(
+      excel[i, "successorNumbers"]
+    )
+    que[["technicalRespresentation"]] <- new.technical_representation(
+      excel[i, "technicalRepresentation.type"],
+      excel[i, "technicalRepresentation.language"],
+      excel[i, "technicalRepresentation.source"]
+    )
+    que[["conceptIds"]] <- list_attribute(
+      excel[i, "conceptIds"]
+    )
+
+    instrument_directory <- file.path(output_directory,
+      paste0("ins", excel[i, "instrumentNumber"]))
+    if (!dir.exists(instrument_directory)) {
+      dir.create(instrument_directory)
+    }
+    # json export
+    question_json <- file.path(
+      instrument_directory,
+      paste0(excel[i, "questionNumber"], ".json")
+    )
+    message("Write question json:", question_json)
+    con <- file(
+      description = question_json,
+      open = "w", encoding = "UTF-8"
+    )
+
+    write(jsonlite::toJSON(nested_env_as_list(que),
+      null = "null", na = "null",
+      pretty = TRUE
+    ), file = con)
+    close(con)
+  }
+  message("Finished writing question jsons\n")
+}
+
+write_question_images <- function(xlsx_file, input_directory,
+  output_directory, images_subdirectory) {
+  message("Read excel file: sheet images\n")
+  excel <- read_and_trim_excel(xlsx_file, sheet = "images")
+
+  # for all images in excel
+  for (i in rownames(excel)) {
+    images_directory <- file.path(output_directory,
+      paste0("ins", excel[i, "instrumentNumber"]),
+      "images", excel[i, "questionNumber"])
+    if (!dir.exists(images_directory)) {
+      dir.create(images_directory)
+    }
+
+    # copy the image file
+    question_image <- paste(input_directory, images_subdirectory,
+       paste0("ins", excel[i, "instrumentNumber"]), excel[i, "fileName"],
+        sep = .Platform$file.sep)
+    message("Copy image file:", question_image)
+    file.copy(
+      question_image,
+      file.path(output_directory,
+        paste0("ins", excel[i, "instrumentNumber"]),
+        "images", excel[i, "questionNumber"],
+      paste0(
+        excel[i, "questionNumber"], "_", excel[i, "indexInQuestion"], ".png")))
+    image <- new.env(hash = TRUE, parent = emptyenv())
+    image[["language"]] <- jsonlite::unbox(excel[i, "language"])
+    image[["containsAnnotations"]] <- jsonlite::unbox(
+      as.logical(excel[i, "containsAnnotations"])
+    )
+    image[["indexInQuestion"]] <- jsonlite::unbox(
+      as.numeric(excel[i, "indexInQuestion"])
+    )
+    image[["resolution"]] <- new.resolution(
+      excel[i, "resolution.widthX"],
+      excel[i, "resolution.heightY"]
+    )
+
+    # json export
+    question_image_json <- file.path(output_directory,
+      paste0("ins", excel[i, "instrumentNumber"]),
+      "images", excel[i, "questionNumber"],
+      paste0(
+        excel[i, "questionNumber"], "_", excel[i, "indexInQuestion"], ".json"))
+    message("Write question image json:", question_image_json)
+    con <- file(question_image_json, "w", encoding = "UTF-8")
+    write(jsonlite::toJSON(nested_env_as_list(image),
+      null = "null", na = "null",
+      pretty = TRUE
+    ), file = con)
+    close(con)
+  }
+  message("Finished writing image files")
+}
+
+trim_cols <- function(x) {
+  gsub("\\s+", "", x)
+}
+
+trim_list_cols <- function(excel, ...) {
+  # delete all whitespaces
+  cols <- list(...)
+  for (i in 1:length(cols)) {
+    excel[[cols[[i]]]] <- trim_cols(excel[[cols[[i]]]])
+  }
+  return(excel)
+}
+
+list_attribute <- function(str_attr) {
+  attr <- unlist(strsplit(str_attr, ",", fixed = TRUE))
+  if (length(attr) > 1 || is.na(attr) == FALSE) {
+    return(attr)
+  }
+}
+
+new.i18n_string <- function(de = NULL, en = NULL) {
+  i18n_string <- new.env(hash = TRUE, parent = emptyenv())
+  i18n_string[["de"]] <- jsonlite::unbox(ifelse(length(de) != 0, de, NULL))
+  i18n_string[["en"]] <- jsonlite::unbox(ifelse(length(en) != 0, en, NULL))
+  return(i18n_string)
+}
+
+new.resolution <- function(widthX = NULL, heightY = NULL) {
+  resolution <- new.env(hash = TRUE, parent = emptyenv())
+  resolution[["widthX"]] <-
+    jsonlite::unbox(ifelse(length(widthX) != 0, as.numeric(widthX), NULL))
+  resolution[["heightY"]] <-
+    jsonlite::unbox(ifelse(length(heightY) != 0, as.numeric(heightY), NULL))
+  return(resolution)
+}
+
+new.technical_representation <- function(technicalRepresentation.type,
+  technicalRepresentation.language,
+  technicalRepresentation.source) {
+  technical_representation <- new.env(hash = TRUE, parent = emptyenv())
+  technical_representation[["type"]] <-
+    jsonlite::unbox(technicalRepresentation.type)
+  technical_representation[["language"]] <-
+    jsonlite::unbox(technicalRepresentation.language)
+  technical_representation[["source"]] <-
+    jsonlite::unbox(technicalRepresentation.source)
+  return(technical_representation)
+}
+
+nested_env_as_list <- function(env) {
+  out <- as.list(env)
+  lapply(out, function(x) if (is.environment(x) ||
+    is.list(x)) nested_env_as_list(x) else x)
+}
---
+++ R/convert_zofar_export_to_handcrafted_questionnaire.R
@@ -0,0 +1,235 @@
+#' Zofar Converter
+#'
+#' This script takes the export of zofar and converts it into a format wich can
+#' be manually edited. The zofar export must have the following structure:
+#' |--ins1
+#'   |--5.json
+#'   |--images
+#'      |--5
+#'         |--5_1.png
+#'         |--5_1.json
+#'         |--5_2.png
+#'         |--5_2.json
+#'         |--5_3.png
+#'         |--5_3.json
+#'
+#' The output directory will be created or overwritten.
+#'
+#' @param input_directory Input path, e.g. "./questions/ins1", must contain
+#' "ins{number}""
+#' @param output_directory Output directory, e.g. "./handcrafted/questions",
+#' will be created if it does not exist or will be overwritten otherwise
+#' @export
+convert_zofar_export_to_handcrafted_questionnaire <- function(input_directory,
+  output_directory = file.path(".", "handcrafted", "questions")) {
+  create_empty_directory(output_directory)
+
+  instrument_number <- parse_instrument_number(input_directory)
+
+  copy_image_files(input_directory, output_directory, instrument_number)
+
+  sheets <- list(
+    "questions" = create_questions_sheet(input_directory, instrument_number),
+    "images" = create_images_sheet(input_directory, instrument_number)
+  )
+
+  excel_file <- file.path(output_directory,
+    paste0("questions-ins", instrument_number, ".xlsx"))
+  message("Write excel: ", excel_file)
+  openxlsx::write.xlsx(sheets,
+    file = excel_file,
+    row.names = FALSE, showNA = FALSE)
+}
+
+copy_image_files <- function(input_directory, output_directory,
+  instrument_number) {
+  images_directory <- file.path(output_directory, "Bilder", "png",
+    paste0("ins", instrument_number))
+  create_empty_directory(images_directory)
+  question_image_pngs <- dir(
+    file.path(input_directory, "images"), pattern = "*.png",
+    recursive = TRUE, full.names = TRUE)
+  for (i in 1:length(question_image_pngs)) {
+    message("Copy image '", question_image_pngs[[i]], "' to directory '",
+      images_directory, "'")
+    file.copy(question_image_pngs[[i]], images_directory)
+  }
+}
+
+create_empty_directory <- function(directory) {
+  if (dir.exists(directory)) {
+    unlink(directory, recursive = TRUE)
+  }
+  dir.create(directory, recursive = TRUE)
+}
+
+parse_instrument_number <- function(input_directory) {
+  return(gsub(input_directory, pattern = "^.*ins", replacement = ""))
+}
+
+create_questions_sheet <- function(input_directory, instrument_number) {
+  question_jsons <- dir(input_directory, pattern = "*.json")
+  col_names <- c("indexInInstrument",
+  "questionNumber",
+  "instrumentNumber",
+  "successorNumbers",
+  "questionText.de",
+  "questionText.en",
+  "instruction.de",
+  "instruction.en",
+  "introduction.de",
+  "introduction.en",
+  "type.de",
+  "type.en",
+  "topic.de",
+  "topic.en",
+  "technicalRepresentation.type",
+  "technicalRepresentation.language",
+  "technicalRepresentation.source",
+  "additionalQuestionText.de",
+  "additionalQuestionText.en",
+  "annotations.de",
+  "annotations.en",
+  "conceptIds")
+  excel_sheet <- data.frame(matrix(ncol = length(col_names),
+    nrow = length(question_jsons)))
+  colnames(excel_sheet) <- col_names
+
+  for (i in 1:length(question_jsons)) {
+    message("Read question json: ", question_jsons[[i]])
+    json <- jsonlite::fromJSON(
+      file.path(input_directory, question_jsons[[i]]))
+
+    excel_sheet$indexInInstrument[i] <- json$indexInInstrument
+    excel_sheet$questionNumber[i] <- gsub(question_jsons[[i]],
+      pattern = ".json", replacement = "")
+    excel_sheet$instrumentNumber[i] <- instrument_number
+    excel_sheet$successorNumbers[i] <- ifelse(
+      length(json$successorNumbers) == 0,
+      NA_character_, paste0(json$successorNumbers, collapse = ","))
+    excel_sheet$questionText.de[i] <- ifelse(length(json$questionText$de) == 0,
+      NA_character_, json$questionText$de)
+    excel_sheet$questionText.en[i] <- ifelse(length(json$questionText$en) == 0,
+      NA_character_, json$questionText$en)
+    excel_sheet$instruction.de[i] <- ifelse(length(json$instruction$de) == 0,
+      NA_character_, json$instruction$de)
+    excel_sheet$instruction.en[i] <- ifelse(length(json$instruction$en) == 0,
+      NA_character_, json$instruction$en)
+    excel_sheet$introduction.de[i] <- ifelse(length(json$introduction$de) == 0,
+      NA_character_, json$introduction$de)
+    excel_sheet$introduction.en[i] <- ifelse(length(json$introduction$en) == 0,
+      NA_character_, json$introduction$en)
+    excel_sheet$type.de[i] <- json$type$de
+    excel_sheet$type.en[i] <- json$type$en
+    excel_sheet$topic.de[i] <- ifelse(length(json$topic$de) == 0,
+      NA_character_, json$topic$de)
+    excel_sheet$topic.en[i] <- ifelse(length(json$topic$en) == 0,
+      NA_character_, json$topic$en)
+    excel_sheet$technicalRepresentation.type[i] <-
+      json$technicalRepresentation$type
+    excel_sheet$technicalRepresentation.language[i] <-
+      json$technicalRepresentation$language
+    excel_sheet$technicalRepresentation.source[i] <-
+      json$technicalRepresentation$source
+    excel_sheet$additionalQuestionText.de[i] <- ifelse(
+      length(json$additionalQuestionText$de) == 0,
+      NA_character_, json$additionalQuestionText$de)
+    excel_sheet$additionalQuestionText.en[i] <- ifelse(
+      length(json$additionalQuestionText$en) == 0,
+      NA_character_, json$additionalQuestionText$en)
+    excel_sheet$annotations.de[i] <- ifelse(
+      length(json$annotations$de) == 0,
+      NA_character_, json$annotations$de)
+    excel_sheet$annotations.en[i] <- ifelse(
+      length(json$annotations$en) == 0,
+      NA_character_, json$annotations$en)
+    excel_sheet$conceptIds <- ifelse(
+      length(json$conceptIds) == 0,
+      NA_character_, json$conceptIds)
+  }
+  # sort by indexInInstrument
+  sorted_excel <- excel_sheet[order(excel_sheet$indexInInstrument), ]
+
+  return(sorted_excel)
+}
+
+create_images_sheet <- function(input_directory, instrument_number) {
+  question_image_jsons <- dir(
+    file.path(input_directory, "images"), pattern = "*.json",
+    recursive = TRUE)
+  col_names <- c("fileName",
+  "questionNumber",
+  "instrumentNumber",
+  "language",
+  "containsAnnotations",
+  "indexInQuestion",
+  "resolution.widthX",
+  "resolution.heightY")
+  excel_sheet <- data.frame(matrix(ncol = length(col_names),
+    nrow = length(question_image_jsons)))
+  colnames(excel_sheet) <- col_names
+
+  for (i in 1:length(question_image_jsons)) {
+    message("Read image json: ", question_image_jsons[[i]])
+    json <- jsonlite::fromJSON(file.path(input_directory, "images",
+      question_image_jsons[[i]]))
+
+    # zofar delivers the field 'file' instead of 'fileName',
+    # furthermore there are path delimiters which we need to be stripped of
+    # generally this field is not required at all for the MDM
+    excel_sheet$fileName[i] <- parse_image_filename(json)
+
+    # generally this field is not required at all for the MDM
+    excel_sheet$questionNumber[i] <- parse_question_number(
+      question_image_jsons[[i]])
+
+    # generally this field is not required at all for the MDM
+    excel_sheet$instrumentNumber[i] <- instrument_number
+
+    excel_sheet$language[i] <- ifelse(length(json$language) == 0,
+      NA_character_, json$language)
+    excel_sheet$containsAnnotations[i] <- ifelse(
+      length(json$containsAnnotations) == 0,
+      NA_character_,
+      json$containsAnnotations)
+    excel_sheet$indexInQuestion[i] <- parse_index_in_question(json,
+      question_image_jsons[[i]])
+    excel_sheet$resolution.widthX[i] <- parse_resolution_width(json)
+    excel_sheet$resolution.heightY[i] <- parse_resolution_height(json)
+  }
+  # sort by questionNumber
+  sorted_excel <- excel_sheet[order(excel_sheet$questionNumber), ]
+
+  return(sorted_excel)
+}
+
+parse_resolution_width <- function(image_json) {
+  return(ifelse(length(image_json$resolution) == 0,
+    NA_character_,
+    gsub(image_json$resolution, pattern = "x.*$", replacement = "")))
+}
+
+parse_resolution_height <- function(image_json) {
+  return(ifelse(length(image_json$resolution) == 0,
+    NA_character_,
+    gsub(image_json$resolution, pattern = "^.*x", replacement = "")))
+}
+
+parse_index_in_question <- function(image_json, json_filename) {
+  ifelse(length(image_json$indexInQuestion) == 0,
+    gsub(gsub(json_filename, pattern = "^.*_",
+      replacement = ""), pattern = ".json", replacement = ""),
+    image_json$indexInQuestion)
+}
+
+parse_image_filename <- function(image_json) {
+  gsub(ifelse(length(image_json$fileName) == 0,
+    ifelse(length(image_json$file) == 0, NA_character_, image_json$file),
+    image_json$fileName), pattern = "^.*/", replacement = "")
+}
+
+parse_question_number <- function(filename) {
+  return(gsub(gsub(filename,
+    pattern = "_[0-9]*.json", replacement = ""),
+    pattern = paste0(.Platform$file.sep, ".*$"), replacement = ""))
+}
