eaae88aa48c5bda78674c49d1029dc27a38e2a97
René Reitmann <reitmann@dzhw.eu>
2021-06-07 12:40:53 +0200

    workaround unc warning (dzhw/metadatamanagement#2884)

--- bin/convert_zofar_to_handcrafted_format.bat
+++ bin/convert_zofar_to_handcrafted_format.bat
@@ -1,6 +1,8 @@
 @echo off
-
+:: remember network path to workaround UNC warning
+set batch_path=%~dp0
+:: replace backslash with slash
+set batch_path=%batch_path:\=/%
 :: run the function in the current directory (should be "01_zofar-format")
-Rscript -e "questionMetadataPreparation::convert_zofar_export_to_handcrafted_questionnaire()"
-
+Rscript -e "setwd('%batch_path%')" -e "questionMetadataPreparation::convert_zofar_export_to_handcrafted_questionnaire()"
 PAUSE
